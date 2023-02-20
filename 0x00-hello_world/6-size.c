e4058c613f3c789c665d31f627418f0333d89bd2
kroozes <nnachigodswill0@gmail.com>
2023-02-20 14:18:10 -0800

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,17 @@
-#include <stdio.h>                              /**                                              * main - entry point                            * @sizeof: size of type                         *                                               * Description: To check size of char, int, long * int, long long int and float types.           */                                             int main(void)                                  {                                                       char a;                                         int b;                                          int c;
-        int d;                                          float e;                                                                                        printf("size of a char: %ld byte(s)\n", sizeof(a));
-        printf("size of an int: %ld byte(s)\n", sizeof(b));
-	printf("size of a long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a long long int: %ld byte(s)\n", sizeof(d));
-	printf("size of a float: %ld byte(s)\n",sizeof(a));
+#include <stdio.h>
+/**
+ * main - entry point
+ * @sizeof: size of type
+ *
+ * description: size of char, int, long int, long long int, float
+ * return: always 0
+ */
+int main(void)
+{
+        printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
 	return 0;
 }
